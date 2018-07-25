0ff88a9a63b7fbce8b2b259662ff623564343c30
Maximrikel <saevhern@gmail.com>
2018-07-25 10:09:53 +0300

    Added cropExample

---
+++ cropExample/App.js
@@ -0,0 +1,48 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+type Props = {};
+
+const multiply = (n1: number, n2: number): number => 
+  {
+    return n1 * n2;
+  }
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text>{multiply(14, 5)}</Text>
+      </View>
+    );
+  }
+
+  
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
